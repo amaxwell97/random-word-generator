c8a305ffbb0becf5cba3bb35d97bf53bb09dd1f7
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-28 17:17:22 -0600

    Declare and set API keys, headlines, and details

---
+++ app.js
@@ -0,0 +1,8 @@
+const dictionaryApiKey = 'ac4c167d-3e4f-49e3-a1e0-2559685b5edd';
+const thesaurusApiKey = 'c271b055-4fb7-4211-bec3-332030a68585';
+
+const todaysDateTitle = document.querySelector('.container h1');
+const todaysWordDisplay = document.querySelector('.container h2');
+const todaysWordDfnDisplay = document.querySelector('#definition');
+const todaysWordSentenceDisplay = document.querySelector('#sentence');
+
