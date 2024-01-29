de025905641eeba13c614a98165bb2ab8dd71502
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-28 18:03:02 -0600

    Declare url and param and create getWordOfDay()

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/';
+const param = '?key=';
 const dictionaryApiKey = 'ac4c167d-3e4f-49e3-a1e0-2559685b5edd';
 const thesaurusApiKey = 'c271b055-4fb7-4211-bec3-332030a68585';
 
@@ -7,7 +9,7 @@ const todaysWordDfnDisplay = document.querySelector('#definition');
 const todaysWordSentenceDisplay = document.querySelector('#sentence');
 
 const today = new Date();
-const dd = today.getDay();
+const dd = today.getDate();
 const month = today.toLocaleString('default', {month: 'long'});
 const yyyy = today.getFullYear();
 const todaysDate = `${month} ${dd}, ${yyyy}`;
@@ -20,3 +22,10 @@ window.addEventListener('load', () => {
     updateDate();
 })
 
+
+
+async function getWordOfDay() {
+
+    const endpoint = `${url}${randomWord}${param}${dictionaryApiKey}`;
+
+}
