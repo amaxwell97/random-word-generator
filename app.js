03456ac8ffef021283361662d83dfac92479ec0d
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-29 21:41:25 -0600

    Declare variables for API Ninja key and url

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const dictionaryParam = '?key=';
 const dictionaryApiKey = 'ac4c167d-3e4f-49e3-a1e0-2559685b5edd';
 const thesaurusApiKey = 'c271b055-4fb7-4211-bec3-332030a68585';
 
-
+const apiNinjaUrl = 'https://api.api-ninjas.com/v1/randomword';
+const ninjaApiKey = 'OU3EgLPnojM0hixoYAg4BQ==7prR9bGUFIJStBV9';
 
 const todaysDateTitle = document.querySelector('.container h1');
 const todaysWordDisplay = document.querySelector('.container h2');
