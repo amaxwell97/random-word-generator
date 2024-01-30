cd118a596f39eebc508fdb1286f28d2dae551588
Avery Maxwell <averymaxwell42@gmail.com>
2024-01-29 21:32:11 -0600

    Update API variable names

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/';
-const param = '?key=';
+const dictionaryUrl = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/';
+const dictionaryParam = '?key=';
 const dictionaryApiKey = 'ac4c167d-3e4f-49e3-a1e0-2559685b5edd';
 const thesaurusApiKey = 'c271b055-4fb7-4211-bec3-332030a68585';
+
+
 
 const todaysDateTitle = document.querySelector('.container h1');
 const todaysWordDisplay = document.querySelector('.container h2');
